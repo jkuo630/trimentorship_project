14c2e52ec143386e299525cfc09a47fcced51dde
jasonkuoubc <jason.kuo@ubc.ca>
2025-02-23 19:55:30 -0800

    task breakdown

--- app.py
+++ app.py
@@ -1,43 +1,58 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-
 # Create Flask's `app` object
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+# structure of session 
+session = {
+    'topic': str,
+    'time': int,
+    'app': str,
+    'status': bool,
+}
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+# user config endpoint - post
+# anthony 
+# make a local array to temp store things 
+# we want this endpoint to be able to add new users and their info to this local array 
+# we want to return, the full list of users 
+@app.route("/userconfig")
+def temp():
+    return "Hello World!"
 
-leaderboard = [
-    {
-        "pfp": "/pfps/jason.jpg",
-        "username": "jkuo630",
-        "score": 1200,
-        "location": "UBC",
-        "postTime": "2hr ago",
-        "postMedia": "/jason.mp4",
-    },
-    {
-        "pfp": "/pfps/jay.png",
-        "username": "therealjaypark",
-        "score": 980,
-        "location": "SFU",
-        "postTime": "3hr ago",
-        "postMedia": "/jay.mp4",
-    },
-]
+# register callback endpoint - post 
+# jason
+@app.route("/registercallback")
+def temp():
+    return "Hello World!"
 
-@app.route("/leaderboard", methods=["GET"])
-def get_leaderboard():
-    """Fetch the current leaderboard."""
-    print("DOING THIS")
-    response = jsonify(leaderboard)
-    response.headers["Content-Type"] = "application/json"
-    return response
+# startSession endpoint - post
+# jason
+@app.route("/start")
+def temp():
+    return "Hello World!"
+
+# endSession endpoint - post
+# anthony 
+# update data and turn status into False
+# return that session 
+@app.route("/end")
+def temp():
+    return "Hello World!"
+
+# currentSession endpoint - get 
+# anthony 
+# make a temp session array/json structure where we store all the sessions 
+# return the session that is currently active 
+@app.route("/current")
+def temp():
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
